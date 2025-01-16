6bde61d6854f2706edc8fac7cbddaae4b0cd1e41
amenabe22 <bdere12345@gmail.com>
2025-01-16 21:27:34 +0300

    remove configs

--- src/core/config.py
+++ src/core/config.py
@@ -4,3 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
